786f92257436f28d55df677ec4c48de089c0e9d1
Kate Stone <Katestone345@gmail.com>
2024-10-07 16:57:57 -0400

    Solution and tests created to run merge alternatively

---
+++ challenges/mergeAlternatively_test.go
@@ -0,0 +1,25 @@
+package challenges_test
+
+import (
+	"github.com/leetcode75/challenges"
+	"testing"
+)
+
+func TestMergeAlternately(t *testing.T) {
+	var result string
+
+	result = challenges.MergeAlternately("abc", "pqr")
+	if result != "apbqcr" {
+		t.Fatalf(`result = %s; want "apbqcr"`, result)
+	}
+
+	result = challenges.MergeAlternately("ab", "pqrs")
+	if result != "apbqrs" {
+		t.Fatalf(`result = %s; want "apbqrs", result`, result)
+	}
+
+	result = challenges.MergeAlternately("abcd", "pq")
+	if result != "apbqcd" {
+		t.Fatalf(`result = %s; want "apbqcd", result`, result)
+	}
+}
